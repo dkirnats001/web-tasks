8f269a1ce1cb2094324b9c82d0db9e1dee4558ca
dkirnats001 <dkirnats@gmail.com>
2026-06-09 00:43:54 +0300

    Add WeatherWidget with TanStack Query: weather GET and POST mutation with invalidation

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import WeatherWidget from './components/WeatherWidget';
+import { Container, Typography } from '@mui/material';
 
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" textAlign="center" sx={{ my: 3 }}>
+        Погодный виджет
+      </Typography>
+      <WeatherWidget />
+    </Container>
+  );
+}
+
+export default App;
